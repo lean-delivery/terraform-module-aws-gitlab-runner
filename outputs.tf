13988c7cd6fca4172ff045cae0f3681653692bb3
Katsiaryna Marozava <Katsiaryna_Marozava@epam.com>
2019-01-28 23:28:39 +0300

    add in outputs iam_bucket_role

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "gitlab_runner_role" {
   value       = "${aws_iam_role.instance.name}"
   description = "Role for gitlabrunners"
 }
+
+output "iam_bucket_role" {
+  value       = "${aws_iam_role.iam_bucket.name}"
+  description = "Role for gitlabrunners"
+}
