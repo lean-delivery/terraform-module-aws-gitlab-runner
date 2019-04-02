fea0291cb8c482bf6ed46e2c3c006fb255bb0e75
aliaksei maiseyeu <jetfire6@gmail.com>
2019-04-02 12:33:28 +0300

    add gitlab_docker_machine_security_group_id to output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "gitlab_runner_security_group_id" {
   value       = "${aws_security_group.runner.id}"
   description = "Runner's security group ID"
 }
+
+output "gitlab_docker_machine_security_group_id" {
+  value       = "${aws_security_group.docker_machine.id}"
+  description = "Docker machine security group ID"
+}
